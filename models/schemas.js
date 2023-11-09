5ddf541d61502056564b801a9145fd74ff3b4206
Elijah Rosario <elijahnicolo@outlook.com>
2023-11-09 16:30:10 +0800

    Added PRODUCT and HOUSE schemas to database
    schemas.js update

--- models/schemas.js
+++ models/schemas.js
@@ -25,10 +25,85 @@ var memberSchema = new mongoose.Schema({    // defines the schema / model for th
     accountType:{                           // Determines if account is an Affiliate, Merchant, or a Shopper
         type: String,
         required: true
+    },
+
+    transactionHistory:{                    // If shopper, this is an Object Array representing transaction history. Each object is a Product ID and date purchased.
+        type: Array,
+    },
+    sellingProducts:{                       // If affiliate, this is an Object Array representing items being sold at their house. Each object is a Product ID and its sale statistics.
+        type: Array,
+    },
+    points:{
+        type: Number,                       // If shopper, this shows number of points user has
     }
 });
 
+
+var productSchema = new mongoose.Schema({    // defines the schema / model for products being sold in the application
+    productID: {                             // Used as the unique identifier (ID) for each product
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    productName:{                           // Name of the product
+        type: String,
+        required: true,
+    },
+    cost:{                           // Determines the price of the product
+        type: Number,
+        required:true
+    },
+    variations: {               // Array of the variations available for the product, if any
+        type: Array,
+    },
+    merchantBrand: {                          // Username of the merchant it belongs to.
+        type: String,
+        required: true
+    },
+    picture: {                                   // Picture of the product
+        data: Buffer,
+        contentType: String,
+    },
+    discount:{                           // Determines the discount of the product, if any
+        type: Number,
+    },
+    sponsoredBy :{                           // Array of all affiliates (represented by their usernames) associated with the product
+        type: Array,
+        required: true
+    }
+});
+
+var houseSchema = new mongoose.Schema({    // defines the schema / model for each house in the application
+    houseID: {                             // Used as the unique identifier (ID) for each house
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    ownerName: {    // Username of owner
+        type: String,
+        required: true,
+    },
+    houseName: {                          // Name displayed on profiles / shops
+        type: String,
+        trim: true,
+        required: true
+    },
+    sponsors :{                           // Array of all merchants (represented by their usernames) associated with the products inside the house
+        type: Array,
+        required: true
+    }
+});
+
+
 memberSchema.plugin(mongoose_fuzzy_searching, { fields: ['displayName'] }); // Add search functionality to a Member's displayName
+productSchema.plugin(mongoose_fuzzy_searching, { fields: ['productName', 'merchantBrand'] }); 
+houseSchema.plugin(mongoose_fuzzy_searching, { fields: ['ownerName', 'houseName'] }); 
+
 
 const Member = mongoose.model('Member', memberSchema);                      // Automatically creates collection called "Members" in database
-module.exports = { Member };                                                // Export schemas
+const Product = mongoose.model('Product', productSchema);
+const House = mongoose.model('House', houseSchema);
+
+module.exports = { Member, Product, House };                                                // Export schemas
